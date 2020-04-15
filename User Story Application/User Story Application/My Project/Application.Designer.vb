afc15464c645a44b9ba0497b193c7107f5df60e9
Michael <218046131@stu.ukzn.ac.za>
2020-04-15 13:41:12 +0200

    Changed Main Form name and text

--- User Story Application/User Story Application/My Project/Application.Designer.vb
+++ User Story Application/User Story Application/My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
 
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()> _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.User_Story_Application.Form1
+            Me.MainForm = Global.User_Story_Application.FormMainMenu
         End Sub
     End Class
 End Namespace
