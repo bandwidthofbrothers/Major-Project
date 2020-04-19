fdf532e04b99fbd5264b221eebaed91467ebbcd8
Michael <218046131@stu.ukzn.ac.za>
2020-04-19 14:34:39 +0200

    Coded Exit Button

--- User Story Application/User Story Application/Interface/Main Menu.vb
+++ User Story Application/User Story Application/Interface/Main Menu.vb
@@ -16,4 +16,8 @@
             .Show()
         End With
     End Sub
+
+    Private Sub ExitToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles ExitToolStripMenuItem.Click
+        Me.Close()
+    End Sub
 End Class
