c4fd787d739038268a373a55d2744f8363a13e62
Zaahira <218019182@stu.ukzn.ac.za>
2020-04-26 20:55:17 +0200

    New form

--- User Story Application/User Story Application/Interface/AddTestCase.Designer.vb
+++ User Story Application/User Story Application/Interface/AddTestCase.Designer.vb
@@ -22,53 +22,53 @@ Partial Class AddTestCase
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        Me.UserStory = New System.Windows.Forms.ListBox()
-        Me.TestCases = New System.Windows.Forms.ListBox()
-        Me.TestCaseBox = New System.Windows.Forms.TextBox()
-        Me.TestCaseBtn = New System.Windows.Forms.Button()
+        Me.UserStoryBox = New System.Windows.Forms.ListBox()
+        Me.TestCaseBox = New System.Windows.Forms.ListBox()
+        Me.TestCaseTextbox = New System.Windows.Forms.TextBox()
+        Me.NewTestCaseBtn = New System.Windows.Forms.Button()
         Me.SuspendLayout()
         '
-        'UserStory
+        'UserStoryBox
         '
-        Me.UserStory.FormattingEnabled = True
-        Me.UserStory.Location = New System.Drawing.Point(34, 53)
-        Me.UserStory.Name = "UserStory"
-        Me.UserStory.Size = New System.Drawing.Size(216, 277)
-        Me.UserStory.TabIndex = 0
-        '
-        'TestCases
-        '
-        Me.TestCases.FormattingEnabled = True
-        Me.TestCases.Location = New System.Drawing.Point(292, 53)
-        Me.TestCases.Name = "TestCases"
-        Me.TestCases.Size = New System.Drawing.Size(263, 121)
-        Me.TestCases.TabIndex = 1
+        Me.UserStoryBox.FormattingEnabled = True
+        Me.UserStoryBox.Location = New System.Drawing.Point(28, 53)
+        Me.UserStoryBox.Name = "UserStoryBox"
+        Me.UserStoryBox.Size = New System.Drawing.Size(220, 277)
+        Me.UserStoryBox.TabIndex = 0
         '
         'TestCaseBox
         '
-        Me.TestCaseBox.Location = New System.Drawing.Point(292, 193)
+        Me.TestCaseBox.FormattingEnabled = True
+        Me.TestCaseBox.Location = New System.Drawing.Point(301, 53)
         Me.TestCaseBox.Name = "TestCaseBox"
-        Me.TestCaseBox.Size = New System.Drawing.Size(263, 20)
-        Me.TestCaseBox.TabIndex = 2
+        Me.TestCaseBox.Size = New System.Drawing.Size(183, 56)
+        Me.TestCaseBox.TabIndex = 1
         '
-        'TestCaseBtn
+        'TestCaseTextbox
         '
-        Me.TestCaseBtn.Location = New System.Drawing.Point(384, 240)
-        Me.TestCaseBtn.Name = "TestCaseBtn"
-        Me.TestCaseBtn.Size = New System.Drawing.Size(110, 38)
-        Me.TestCaseBtn.TabIndex = 3
-        Me.TestCaseBtn.Text = "Add New Test Case"
-        Me.TestCaseBtn.UseVisualStyleBackColor = True
+        Me.TestCaseTextbox.Location = New System.Drawing.Point(301, 146)
+        Me.TestCaseTextbox.Name = "TestCaseTextbox"
+        Me.TestCaseTextbox.Size = New System.Drawing.Size(183, 20)
+        Me.TestCaseTextbox.TabIndex = 2
+        '
+        'NewTestCaseBtn
+        '
+        Me.NewTestCaseBtn.Location = New System.Drawing.Point(333, 201)
+        Me.NewTestCaseBtn.Name = "NewTestCaseBtn"
+        Me.NewTestCaseBtn.Size = New System.Drawing.Size(126, 45)
+        Me.NewTestCaseBtn.TabIndex = 3
+        Me.NewTestCaseBtn.Text = "Add New Test Case"
+        Me.NewTestCaseBtn.UseVisualStyleBackColor = True
         '
         'AddTestCase
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(639, 367)
-        Me.Controls.Add(Me.TestCaseBtn)
+        Me.ClientSize = New System.Drawing.Size(743, 396)
+        Me.Controls.Add(Me.NewTestCaseBtn)
+        Me.Controls.Add(Me.TestCaseTextbox)
         Me.Controls.Add(Me.TestCaseBox)
-        Me.Controls.Add(Me.TestCases)
-        Me.Controls.Add(Me.UserStory)
+        Me.Controls.Add(Me.UserStoryBox)
         Me.Name = "AddTestCase"
         Me.Text = "AddTestCase"
         Me.ResumeLayout(False)
@@ -76,8 +76,8 @@ Partial Class AddTestCase
 
     End Sub
 
-    Friend WithEvents UserStory As ListBox
-    Friend WithEvents TestCases As ListBox
-    Friend WithEvents TestCaseBox As TextBox
-    Friend WithEvents TestCaseBtn As Button
+    Friend WithEvents UserStoryBox As ListBox
+    Friend WithEvents TestCaseBox As ListBox
+    Friend WithEvents TestCaseTextbox As TextBox
+    Friend WithEvents NewTestCaseBtn As Button
 End Class
