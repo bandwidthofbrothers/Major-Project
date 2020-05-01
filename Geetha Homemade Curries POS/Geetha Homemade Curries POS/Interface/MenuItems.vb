12adf2221d2db983d8a0a5ff998a5cfa9d0b0dec
Zaahira <218019182@stu.ukzn.ac.za>
2020-05-01 14:18:47 +0200

    Dataset

--- Geetha Homemade Curries POS/Geetha Homemade Curries POS/Interface/MenuItems.vb
+++ Geetha Homemade Curries POS/Geetha Homemade Curries POS/Interface/MenuItems.vb
@@ -6,6 +6,8 @@ Public Class MenuItems
     Dim connection As New SqlConnection("Server=STUDENT-250JE01; Database=group22; integrated security = false")
 
     Private Sub MenuItems_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        'TODO: This line of code loads data into the 'Group22DataSet.MenuItem' table. You can move, or remove it, as needed.
+        Me.MenuItemTableAdapter.Fill(Me.Group22DataSet.MenuItem)
         'TODO: This line of code loads data into the 'DataSet.MenuItem' table. You can move, or remove it, as needed.
         Me.MenuItemTableAdapter.Fill(Me.DataSet.MenuItem)
         'TODO: This line of code loads data into the 'DataSet1.CustomerOrder' table. You can move, or remove it, as needed.
